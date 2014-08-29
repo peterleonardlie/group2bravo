2ba7d7b4114c4513765b4dbd278414367f740fd8
peterleonardlie <peter.leonard.lie@gmail.com>
2014-08-29 16:56:26 +0800

    Create adminPage.php

---
+++ adminPage.php
@@ -0,0 +1,106 @@
+<?php
+	session_start();
+?>
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />
+<title>Bravo Hospital | Admin UI</title>
+<link rel="shortcut icon" href="HospLogo.png" type="image/icon">
+<link rel="stylesheet" type="text/css" href="style.css"/>
+</head>
+
+<body class="BackgroundColor">
+
+<div id="wrapper">
+<div id="Menu">
+	<?php
+    	if($_SESSION['login'] == true){
+			echo '<a href="memberDetails.php">'.$_SESSION['username'].'</a> | <a href="logout.php">Logout</a>';
+		}
+		else{
+		echo '<a href="login.php">Login</a> | <a href="signup.php">Register</a>';
+		}
+    ?>
+</div>
+<div class="logobackground">
+</div>
+<div class="logo">
+<img src="HospLogo.png" width="137" height="121" alt="HospLogo" /></div>
+
+<div id="navbar">
+<div id="holder">
+
+<ul>
+<li><a href="index.php">Home</a></li>
+<li><a href="about.php">About</a></li>
+<li><a href="payment.php">Payment</a></li>
+<li><a href="appointment.php">Appointment</a></li>
+<li><a href="contact.php">Contact</a></li>
+
+</ul>
+
+</div><!-- end of holder div -->
+
+</div><!-- end of navbar div -->
+
+<div class="horizontalLineAppt"></div>
+<div class="horizontalLineAppt2"></div>
+<div id="searchBar">
+  <form id="tfnewsearch" method="get" action="http://www.google.com">
+          <input type="text" class="tftextinput" size="25" maxlength="100"><input type="submit" value="search" class="tfbutton">
+</form></div>
+<div id="bottomMenuAdmin" style="z-index:200001"><div id="footerTextAdmin" style="z-index:200001">Copyright(c) 2014-Bravo Hospital. All rights reserved.</div></div>
+
+<div id="addressBox">Bravo Hospital </br>600 Upper Thomson Rd</br> Singapore 574421</div>
+
+</div><!-- end of wrapper div -->
+
+<?php
+	if($_SESSION['admin'] == true){
+		echo'<div id="navbarTop">';
+	} else{
+		echo'<div id="navbarTop" style="left:739px;">';
+	}
+?>
+<div id="holderTop">
+
+<ul>
+<?php
+	if($_SESSION['admin'] == true){
+		echo '<li><a href="adminPage.php" id="onlink">Admin Function</a></li>';
+	}
+?>
+<li><a href="drdirectory.php">Doctor Directory</a></li>
+<li><a href="services.php">Services</a></li>
+
+</ul>
+
+</div><!-- end of holderTop div -->
+
+</div><!-- end of navbarTop div -->
+<div class="Adminbackground"></div>
+<div class="transparentBoxAdmin"><div class="posAdminPage">
+  <h1>Admin Function</h1></div>
+<div id="adminDetails">
+<form action="viewAllAppointment.php">
+    <input type="submit" value="View all appointment" style="width:225px; height:50px; background-color:#FC6">
+</form>
+
+<form action="updateSelect.php">
+    <input type="submit" value="Edit clients appointment" style="width:225px; height:50px; background-color:#FC6">
+</form>
+
+<form action="deleteSelect.php">
+    <input type="submit" value="Cancel clients appointment" style="width:225px; height:50px;background-color:#FC6">
+</form>
+
+</div>
+
+
+
+</div>
+
+</div><!-- end of wrapper div -->
+</body>
+</html>
